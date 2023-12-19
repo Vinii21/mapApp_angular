ec4eec08e1990fc4df8d8cceaf73f52fec19b9f9
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-12-18 22:58:15 -0600

    Marcadores de mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { LngLat, Map, Marker } from 'maplibre-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild("viewDiv")
+  public divMap?: ElementRef;
 
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-118.805, 34.027)
+
+
+  ngAfterViewInit(): void {
+    const apiKey = environment.ARCGIS_KEY;
+    const basemapEnum = "arcgis/streets";
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // the id of the div element
+      style: `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapEnum}?token=${apiKey}`,
+      zoom: 13, // starting zoom
+      center: this.currentLngLat // starting location [longitude, latitude]
+    });
+
+    //Con esto creamos un marcador personalizado para el mapa
+    /* const markerHtml = document.createElement('div');
+    markerHtml.innerHTML = "Vini"; */
+
+    /* const marker = new Marker({
+      color: "red",
+      element: markerHtml
+    })
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map); */
+  }
+
+  createMarker() {
+
+    if(!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lgnLat = this.map.getCenter();
+
+    this.addMarker(lgnLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string): void {
+    if(!this.map) return;
+
+    const marker = new Marker({
+      color,
+      draggable: true,
+    })
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map);
+
+  }
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,18 +1,19 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Map } from 'maplibre-gl';
+import { LngLat, Map } from 'maplibre-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild("viewDiv")
   public divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-118.805, 34.027)
 
 
   ngAfterViewInit(): void {
@@ -22,7 +23,7 @@ export class ZoomRangePageComponent implements AfterViewInit {
       container: this.divMap?.nativeElement, // the id of the div element
       style: `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapEnum}?token=${apiKey}`,
       zoom: this.zoom, // starting zoom
-      center: [-118.805, 34.027] // starting location [longitude, latitude]
+      center: this.currentLngLat // starting location [longitude, latitude]
     });
 
     this.mapListener();
@@ -33,6 +34,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
     this.map.on("zoom", (ev)=>{
       this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on("move", ()=>{
+      this.currentLngLat = this.map!.getCenter();
     })
   }
 
@@ -47,6 +52,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
   zoomChanged(value: string): void {
     this.zoom = Number(value);
     this.map?.zoomTo(this.zoom);
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
 }
