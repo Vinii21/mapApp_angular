9be5883c55a210c2b37073503f30d161a9ab2f96
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-12-28 21:46:39 -0600

    Fly to

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -74,4 +74,11 @@ export class MarkersPageComponent implements AfterViewInit {
     this.markers[index].marker.remove();
     this.markers.splice(index, 1);
   }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat()
+    })
+  }
 }
