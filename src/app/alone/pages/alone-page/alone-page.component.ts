5515aaf2a9116af82527599a09798b34281c7674
Vinicio Chavarria <vichavarria21@gmaail.com>
2024-04-25 07:51:19 -0600

    Tarea de convertir un componente convencional en un standalone component

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   /* selector: 'app-alone-page', */ /* Lo remuevo porque lo quiero cargar de manera perezosa y en las rutas. */
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
